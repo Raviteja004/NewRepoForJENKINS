b90b9ac4eb70d5f99d1bad0847c73dc6816c833c
Raviteja Sripathi <sraviteja004@gmail.com>
2021-03-21 19:48:01 +0530

    FirstCommit for this project for JENKINS

---
+++ FirstMavenProject/src/main/java/in/Ravi/HelloWorld.java
@@ -0,0 +1,11 @@
+package in.Ravi;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java waiting for Jenkins deployement");
+		System.out.println("New comment added");
+
+	}
+
+}
